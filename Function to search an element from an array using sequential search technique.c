35caa564ac090f639d3823bd3809fe4fba90f483
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2022-01-29 08:36:46 +0530

    29-01-2022

--- Function to search an element from an array using sequential search technique.c
+++ Function to search an element from an array using sequential search technique.c
@@ -4,7 +4,7 @@
 #define MAX 10
 void create(int *,int *);
 void display(int *,int);
-void seqsearch(int *,int,int ele);
+void seqsearch(int *,int,int);
 void main()
     {
 	    int a[MAX],n=0,ele,loc;
