c23c74cf7731fb79d1a29f7f5176baa24ba0cea1
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2022-02-24 17:17:59 +0530

    2D Array

---
+++ 1st Diagonal Matrix.c
@@ -0,0 +1,35 @@
+/*Program to print 1st diagonal matrix*/
+#include<stdio.h>
+#include<conio.h>
+void input(int *,int);
+void disp(int *,int);
+void main()
+    {
+        int mat[10],r;
+        printf("Enter row/column of matrix:");
+        scanf("%d",&r);
+        input(mat, r);
+        disp(mat, r);
+        getch();
+    }
+void input(int *a,int r)
+    {
+        int i;
+        printf("\nEnter diagonal(non-zero)elements:");
+        for (i = 0; i < r;i++)
+            scanf("%d", &a[i]);
+    }
+void disp(int *a, int r)
+    {
+        int i, j;
+        printf("\nThe matrix is:\n");
+        for (i = 0; i < r;i++)
+            {
+                for (j = 0; j < r;j++)
+                    if(i==j)
+                        printf("\t%d", a[i]);
+                    else
+                        printf("\t0");
+                printf("\n");
+            }
+    }        
